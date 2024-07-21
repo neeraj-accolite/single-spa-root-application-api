dbc3c6fc84b43f67cf2678005e5d92907439462b
Neeraj Agrawal <neeraj.agrawal@discounttire.com>
2024-07-21 23:20:52 +0530

    initial commit

---
+++ src/acc-root-config.ts
@@ -0,0 +1,41 @@
+import { registerApplication, start, LifeCycles } from "single-spa";
+import { bootstrap, mount, unmount } from './header/acc-header';
+
+registerApplication(
+  'root-app',
+  () => import('./header/acc-header'),
+  () => true,
+  { bootstrap, mount, unmount }
+);
+
+registerApplication({
+  name: "@acc/profile",
+  app: () =>
+    System.import<LifeCycles>(
+      "@acc/profile"
+    ),
+  activeWhen: ["/profiles"],
+});
+
+
+registerApplication({
+  name: "@acc/address",
+  app: () =>
+    System.import<LifeCycles>(
+      "@acc/address"
+    ),
+  activeWhen: ["/address"],
+});
+
+registerApplication({
+  name: "@acc/orders",
+  app: () =>
+    System.import<LifeCycles>(
+      "@acc/orders"
+    ),
+  activeWhen: ["/orders/:id"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
